b8c8c127a28d59bc3b93ee4596dfa87743706010
George Onyango <onyangogeorge954@gmail.com>
2020-08-28 17:57:50 +0300

    changes are irrelevant

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
-    $(".lik").click(".hide").slidetoggle(1200);
-    $("lik").click(".show").toggle(1500);
+    $('#please').click(function() {
+        $('#jil').toggle();
+    });
+    $('#pale').click(function() {
+        $('#lik').toggle();
+    });
+    $('#mame').click(function() {
+        $('pil').toggle();
+    });
 });
+  
+$("button").click(function(event) {
+    var client = document.getElementById("name").value();
+    alert(' Dear ' + ' esteemed ' + client + ' we have received your message successful. ' + ' Feel free to reach out to us anytime');
+    event.preventDefault();
+});
+
